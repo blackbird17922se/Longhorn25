786a420c747d83021dfd0af88a656459693f457b
blackbird17922se <blackbird17922@gmail.com>
2025-06-08 01:46:06 -0500

    creacion inicial, agrupacion con desarrollo 2022

---
+++ js/main.js
@@ -0,0 +1,127 @@
+(function ($) {
+ "use strict";
+
+	/*----------------------------
+	 jQuery MeanMenu
+	------------------------------ */
+	jQuery('nav#dropdown').meanmenu();	
+	/*----------------------------
+	 jQuery myTab
+	------------------------------ */
+	$('#myTab a').click(function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+		$('#myTab3 a').click(function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+		$('#myTab4 a').click(function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+
+	  $('#single-product-tab a').click(function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+	
+	$('[data-toggle="tooltip"]').tooltip(); 
+	
+
+	// sidebarCollapse: idnetificador del boton que expande o contrae la barra
+	// sidebar: barra lateral
+	$('#sidebarCollapse').on('click', function () {
+                     $('#sidebar').toggleClass('active');
+                     
+                 });
+		// Collapse ibox function
+			$('#sidebar ul li').on('click', function () {
+				var button = $(this).find('i.fa.indicator-mn');
+				button.toggleClass('fa-plus').toggleClass('fa-minus');
+				
+			});
+	/*-----------------------------
+			Menu Stick
+		---------------------------------*/
+		$(".sicker-menu").sticky({topSpacing:0});
+			
+		$('#sidebarCollapse').on('click', function () {
+			$("body").toggleClass("mini-navbar");
+			SmoothlyMenu();
+		});
+		$(document).on('click', '.header-right-menu .dropdown-menu', function (e) {
+			  e.stopPropagation();
+			});
+ 
+	/****************************************** CONTROLES MENU DE INICIO ********************************/
+
+    const menuBtn = document.getElementById('menuCollapse');
+	const menuBox = document.getElementById('box-menu-ini');
+
+
+
+    menuBtn.addEventListener('click', (e) => {
+		e.stopPropagation();
+		menuBox.classList.toggle('active');
+	});
+	
+	document.addEventListener('click', (e) => {
+		if (!menuBox.contains(e.target) && e.target !== menuBtn) {
+			menuBox.classList.remove('active');
+		}
+	});
+	
+
+
+
+
+
+/*----------------------------
+ wow js active
+------------------------------ */
+ new WOW().init();
+ 
+/*----------------------------
+ owl active
+------------------------------ */  
+  $("#owl-demo").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 4,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,4],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ price-slider active
+------------------------------ */  
+	  $( "#slider-range" ).slider({
+	   range: true,
+	   min: 40,
+	   max: 600,
+	   values: [ 60, 570 ],
+	   slide: function( event, ui ) {
+		$( "#amount" ).val( "£" + ui.values[ 0 ] + " - £" + ui.values[ 1 ] );
+	   }
+	  });
+	  $( "#amount" ).val( "£" + $( "#slider-range" ).slider( "values", 0 ) +
+	   " - £" + $( "#slider-range" ).slider( "values", 1 ) );  
+	   
+/*--------------------------
+ scrollUp
+---------------------------- */	
+	$.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    }); 	   
+ 
+})(jQuery); 
