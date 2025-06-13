ff115cc1eaa0f87a95569bca1daf7c441a3dc949
blackbird17922se <blackbird17922@gmail.com>
2025-06-12 22:58:52 -0500

    reloj inicial

--- js/main.js
+++ js/main.js
@@ -90,6 +90,30 @@
   });
 
 
+//   RELOJ
+
+
+  function updateClock() {
+  const now = new Date();
+
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+
+  const secondDeg = second * 6;
+  const minuteDeg = minute * 6 + second * 0.1;
+  const hourDeg = (hour % 12) * 30 + minute * 0.5;
+
+  document.getElementById("second-hand").style.transform = `rotate(${secondDeg}deg)`;
+  document.getElementById("minute-hand").style.transform = `rotate(${minuteDeg}deg)`;
+  document.getElementById("hour-hand").style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
+
+
 
 /*----------------------------
  wow js active
